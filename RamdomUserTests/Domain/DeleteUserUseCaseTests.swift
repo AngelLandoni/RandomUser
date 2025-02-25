a643f9fbaa21a1dbe417dcf37ebe9669b0372003
Angel Landoni <angel.landoni.usa@gmail.com>
2025-02-25 14:52:33 +0100

    Add `DeleteUserUseCaseTests`

--- RamdomUserTests/Domain/DeleteUserUseCaseTests.swift
+++ RamdomUserTests/Domain/DeleteUserUseCaseTests.swift
@@ -1,7 +1,33 @@
-//
-//  Untitled.swift
-//  RamdomUser
-//
-//  Created by Angel Landoni on 25/2/25.
-//
+import XCTest
+@testable import RamdomUser
 
+@MainActor
+final class DeleteUserUseCaseTests: XCTestCase {
+    
+    private var repositoryMock: MockUserRepository!
+    private var deleteUserUseCase: DeleteUserUseCase!
+
+    override func setUp() {
+        super.setUp()
+        repositoryMock = MockUserRepository()
+        deleteUserUseCase = DeleteUserUseCase(repository: repositoryMock)
+    }
+
+    override func tearDown() {
+        repositoryMock = nil
+        deleteUserUseCase = nil
+        super.tearDown()
+    }
+    
+    func testExecute_ShouldCallDeleteAndBanUser() async {
+        // Given
+        let userID = "12345"
+        
+        // When
+        await deleteUserUseCase.execute(id: userID)
+        
+        // Then
+        XCTAssertEqual(repositoryMock.deleteUserCalledWithID, userID, "deleteUser should be called with correct ID")
+        XCTAssertEqual(repositoryMock.banUserCalledWithID, userID, "banUser should be called with correct ID")
+    }
+}
